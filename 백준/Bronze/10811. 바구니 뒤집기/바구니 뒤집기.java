99c659c90bf0c4d35c53109a4ca2847a4a05f2f3
Jinsu Oh <51368746+Laid41798175@users.noreply.github.com>
2023-03-03 00:58:34 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 144 ms, Memory: 15948 KB -BaekjoonHub

---
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
@@ -0,0 +1,65 @@
+
+//Do Not Modify From
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static String str = "";
+    static StringTokenizer stk;
+    static StringBuilder sb = new StringBuilder();
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    static int T;
+    static int n;
+    static int m;
+    // Do Nod Modify To
+
+    public static void main(String[] args) throws IOException {
+        solve();
+        System.out.print(sb);
+    }
+
+    static int[] arr;
+
+    public static void solve() throws IOException {
+        str = br.readLine();
+        stk = new StringTokenizer(str);
+        n = Integer.parseInt(stk.nextToken());
+        m = Integer.parseInt(stk.nextToken());
+        arr = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            arr[i] = i;
+        }
+
+        for (int i = 0; i < m; i++) {
+            str = br.readLine();
+            stk = new StringTokenizer(str);
+            int a = Integer.parseInt(stk.nextToken());
+            int b = Integer.parseInt(stk.nextToken());
+            reverse(a, b);
+        }
+
+        print();
+    }
+
+    static void print(){
+        for (int i = 1; i <= n; i++) {
+            sb.append(arr[i] + " ");
+        }
+        sb.append('\n');
+    }
+
+    static void reverse(int a, int b){
+        for (int i = 0; i <= (b - a) / 2; i++) {
+            swap(a + i, b - i);
+        }
+    }
+
+    static void swap(int a, int b) {
+        int tmp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = tmp;
+    }
+}
