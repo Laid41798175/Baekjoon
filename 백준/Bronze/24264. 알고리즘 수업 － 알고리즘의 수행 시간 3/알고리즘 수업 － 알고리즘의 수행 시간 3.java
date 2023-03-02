b3b4c1ccbc7a94d706cef8484d6c0507fbc4f757
Jinsu Oh <51368746+Laid41798175@users.noreply.github.com>
2023-03-03 01:43:57 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 3, Time: 144 ms, Memory: 14608 KB -BaekjoonHub

---
+++ 백준/Bronze/24264. 알고리즘 수업 － 알고리즘의 수행 시간 3/알고리즘 수업 － 알고리즘의 수행 시간 3.java
@@ -0,0 +1,29 @@
+//Do Not Modify From
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static String str = "";
+    static StringTokenizer stk;
+    static StringBuilder sb = new StringBuilder();
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    static int T;
+    static long n;
+    static int m;
+    // Do Nod Modify To
+
+    public static void main(String[] args) throws IOException {
+        solve();
+        System.out.print(sb);
+    }
+
+    public static void solve() throws IOException {
+        str = br.readLine();
+        n = Integer.parseInt(str);
+        n *= n;
+        sb.append(n+"\n2\n");
+    }
+}
