35fce15df83a8bd90f9a10c0de23cb9b07de3cf8
Jinsu Oh <51368746+Laid41798175@users.noreply.github.com>
2023-03-04 21:01:54 +0900

    [Bronze V] Title: A+B, Time: 100 ms, Memory: 14412 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scn=new Scanner(System.in);
+
+		String as=scn.next(); int a=Integer.parseInt(as);
+		int b=scn.nextInt();
+		
+		System.out.println(a+b);
+	}
+
+}
